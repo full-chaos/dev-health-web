a244fd372b216e303013a1479376b4194429f976
Chris George <chris@noreply.noreply.nope>
2026-01-04 14:46:15 -0800

    Enable runtime backend url

--- next.config.ts
+++ next.config.ts
@@ -20,6 +20,14 @@ const nextConfig: NextConfig = {
     : {
       pageExtensions: ["tsx", "ts", "jsx", "js"],
     }),
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: `${process.env.BACKEND_URL || "http://127.0.0.1:8000"}/api/:path*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
