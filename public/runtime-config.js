47c64337023c2a34d8d4d2f6206cd2e042536c68
Chris George <chrisgeo@users.noreply.github.com>
2026-02-13 06:40:45 -0800

    Delete public/runtime-config.js

--- public/runtime-config.js
+++
@@ -1,2 +0,0 @@
-// Auto-generated at start-up.
-window.__DEV_HEALTH_RUNTIME__ = {"publicEnv":{"NEXT_PUBLIC_USE_GRAPHQL_ANALYTICS":"true","NEXT_PUBLIC_DEV_HEALTH_TEST_MODE":"false","NEXT_PUBLIC_DOCS_URL":"/docs"}};
